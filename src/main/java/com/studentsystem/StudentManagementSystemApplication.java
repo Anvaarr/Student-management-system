d135d3e4f3354cc5ed5b27071e51d83566bc6a2b
Temirlan <Tima200309@gmail.com>
2024-06-11 19:50:46 +0500

    Few changes to the project

--- src/main/java/com/studentsystem/StudentManagementSystemApplication.java
+++ src/main/java/com/studentsystem/StudentManagementSystemApplication.java
@@ -1,13 +1,30 @@
 package com.studentsystem;
 
+import com.studentsystem.entity.Student;
+import com.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public void run(String... args) throws Exception {
 
+//        Student student1 =  new Student("Tim", "Nabokov", "Nabokov@gmail.com");
+//        studentRepository.save(student1);
+//
+//        Student student2 = new Student("Max", "Adreewokov", "Andreewokov@gmail.com");
+//        studentRepository.save(student2);
+//
+//        Student student3 = new Student("Pol", "Jakevuich", "Jakevuich@gmail.com");
+//        studentRepository.save(student3);
+    }
 }
